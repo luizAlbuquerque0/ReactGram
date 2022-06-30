4084822d221bfb692a5d7d6ae538e49f634b664c
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-06-30 19:53:26 -0300

    Adicionando registro de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,7 +13,36 @@ const genereteToken = (id)=>{
 
 //REgister user and sing in
 const register = async(req,res)=>{
-    res.send("Registro")
+    
+    const {name, email,password} = req.body
+
+    const user = await User.findOne({email});
+    if (user){
+        res.status(422).json({errors: ["Por favor, utilizar outro email"]})
+        return
+    }
+
+    //password hash
+    const salt = await bCrypt.genSalt()
+    const passwordHash = await bCrypt.hash(password, salt)
+
+    //create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    if(!newUser){
+        res.status(422).json({errors:["Houve um erro, por favor tente novamente mais tarde"]});
+        return
+    }
+
+    //if user was created successfullu, return the token
+    res.status(201).json({
+        _id: newUser._id,
+        token: genereteToken(newUser._id)
+    });
 }
 
 module.exports = {
