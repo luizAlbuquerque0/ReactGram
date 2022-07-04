c4abefb739a439b2a79539dba284faeb1e4cb62e
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-07-04 03:41:44 -0300

    Resgate de usuŕio por id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -117,9 +117,32 @@ const update = async(req,res)=>{
 }
 
 
+// Get user by id
+const getUserById = async(req,res)=>{
+    const {id} = req.params
+
+    try {
+        const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password")
+
+        if(!user){
+            res.status(404).json({errors: ["Usuário não encontrado"]})
+            return
+        }
+    
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(402).json({errors: ["Usuário não encontrado"]})
+        return
+    }
+
+    
+}
+
+
 module.exports = {
     register,
     login,
     getCurrentUser,
     update,
+    getUserById,
 }
