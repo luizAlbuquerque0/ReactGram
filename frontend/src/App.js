27d0c288a47c88633f0dc8bde804b2056f90521e
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-07-04 06:30:09 -0300

    Componentes navbar e footer e formulario de cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigte } from "react-router-dom";
+
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+//Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>React gram</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
