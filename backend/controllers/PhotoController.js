be4d3eaa3d2050d9403c4b6b33ac3354ec1d92fc
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-07-04 04:38:59 -0300

    Funcionalidade de excluir fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -23,9 +23,45 @@ const insertPhoto = async (req, res) => {
         res.status(422).json({
             erros: ["Houve um problema, por favor tente novamente mais tarde"],
         });
+        return;
     }
 
     res.send(newPhoto);
 };
 
-module.exports = { insertPhoto };
+//Remove a photo from db
+const deletePhoto = async (req, res) => {
+    const { id } = req.params;
+
+    const reqUser = req.user;
+
+    try {
+        const Photo = await photo.findById(mongoose.Types.ObjectId(id));
+
+        if (!Photo) {
+            res.status(404).json({ erros: ["Foto não encontrada"] });
+            return;
+        }
+
+        //Check if photo belongs to user
+        if (!Photo.userId.equals(reqUser._id)) {
+            res.status(404).json({
+                erros: [
+                    "Ocorreu um erro, por favor tente novamente mais tarde",
+                ],
+            });
+        }
+
+        await photo.findByIdAndDelete(Photo._id);
+
+        res.status(200).json({
+            id: Photo._id,
+            message: "Foto excluida com sucesso.",
+        });
+    } catch (error) {
+        res.status(404).json({ erros: ["Foto não encontrada"] });
+        return;
+    }
+};
+
+module.exports = { insertPhoto, deletePhoto };
